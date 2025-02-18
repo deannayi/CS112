31fe8d13aa605456f7c79214df64533829610326
Deanna Yi <deannayi@deannas-macbook-air.local>
2025-02-18 17:40:20 -0500

    numCars

---
+++ L5LinkedTrainCars/test/LinkedTrainCarsTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import singly.*;
+
+/*
+* This is a Java Test Class, which uses the JUnit package to create
+* and run tests
+* 
+* You can use this to evaluate your code. Examine these tests, as writing
+* similar test cases will help you immensly on other Assignments/Labs, as
+* well as moving forward in your CS career.
+*/
+public class LinkedTrainCarsTest {
+
+    @Test
+    public void testNumCars() {
+        // WRITE YOUR CODE HERE
+        LinkedTrainCars test = new LinkedTrainCars();
+
+    }
+
+    @Test
+    public void testInsertAt() {
+        // WRITE YOUR CODE HERE
+        LinkedTrainCars test = new LinkedTrainCars();
+
+    }
+
+}
